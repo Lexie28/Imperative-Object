9c6cd19c79e0ac526332f7265b676dde96c1468b
Alexandra Dahlberg <alexandradahlberg@LAPTOP-230VP7EK.localdomain>
2022-09-11 16:59:17 +0200

    assignment1_step0

---
+++ inluppar/inlupp1/hash_table.c
@@ -0,0 +1,36 @@
+#include "hash_table.h"
+#include <stdio.h>
+#include <stdlib.h>
+#include <stdbool.h>
+#include <string.h>
+#include <ctype.h>
+#include <time.h>
+
+
+typedef struct entry entry_t;
+
+struct entry
+{
+  int key;       // holds the key
+  char *value;   // holds the value
+  entry_t *next; // points to the next entry (possibly NULL)
+};
+
+struct hash_table
+{
+  entry_t *buckets[17]; // varför *? Allocate memory to hold 17 buckets where each bucket is represented as a pointer to an entry_t
+};
+
+
+ioopm_hash_table_t *ioopm_hash_table_create()
+{
+  /// Allocate space for a ioopm_hash_table_t = 17 pointers to
+  /// entry_t's, which will be set to NULL
+  ioopm_hash_table_t *result = calloc(1, sizeof(ioopm_hash_table_t));
+  return result;
+}
+
+void ioopm_hash_table_destroy(ioopm_hash_table_t *ht) {
+  free(ht);
+}
+
