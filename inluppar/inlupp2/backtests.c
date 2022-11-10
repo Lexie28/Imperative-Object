922d705a06be031c75e02cb9d0da979a6d2d6903
Vincent <vincent.gus@hotmail.com>
2022-11-10 14:04:30 +0100

    tests

--- inluppar/inlupp2/backtests.c
+++ inluppar/inlupp2/backtests.c
@@ -28,18 +28,6 @@ void test_db_create()
     ioopm_db_destroy(db);
 }
 
-void test_merch_create()
-{
-    char *name        = "Test merch";
-    char *description = "Very cool";
-    int   price       = 101010;
-    merch_t *test     = create_merch(strdup(name), strdup(description), price);
-
-    CU_ASSERT_PTR_NOT_NULL(test);
-
-    destroy_merch(test);
-}
-
 void test_add_merchandise()
 {
     db_t *db = ioopm_db_create();
@@ -65,8 +53,7 @@ void test_remove_merchandise()
     ioopm_add_merchandise(db, strdup(name), strdup(description), price);
     option_t a = ioopm_hash_table_lookup(ht, ptr_elem(name));
     CU_ASSERT_TRUE(a.success);
-    bool success = false;
-    ioopm_remove_merchandise(db, name, &success);
+    ioopm_remove_merchandise(db, name, false);
     option_t b = ioopm_hash_table_lookup(ht, ptr_elem(name));
     CU_ASSERT_FALSE(b.success);
     ioopm_db_destroy(db);
@@ -149,25 +136,28 @@ void test_replenish_stock()
     CU_ASSERT_TRUE(a.success);
     char *shelftoreplenish = "J44";
     int amount = 10;
-    ioopm_replenish_stock(db, name, shelftoreplenish, amount);
+    ioopm_replenish_stock(db, strdup(name), strdup(shelftoreplenish), amount);
     merch_t *merch = a.value.p;
     ioopm_list_t *locs = merch->locs;
-    bool success = strcmp((name), elem_ptr(ioopm_linked_list_remove(locs, 0).p));
-    CU_ASSERT_TRUE(success);
+    CU_ASSERT_TRUE(strcmp(name, (char *)(ioopm_linked_list_remove(locs, 0)).p));
     ioopm_db_destroy(db);
 }
 
 void test_add_remove_cart()
 {
     db_t *db = ioopm_db_create();
-    char *name = "Lexie";
-    char *description = "Cool kid";
+    char *name = strdup("Lexie");
+    char *description = strdup("Cool kid");
     int price = 55;
 
-    ioopm_add_merchandise(db, strdup(name), strdup(description), price);
+    ioopm_add_merchandise(db, name, description, price);
 
     ioopm_cart_create(db); //Automagically asigns a cart 1 since not other carts exist.
     ioopm_add_to_cart(db, 1, name, 10);
+
+    ioopm_hash_table_t htc = db->carts;
+    ioopm_hash_table_t cart = ioopm_hash_table_lookup(htc, ptr_elem("1"));
+
     CU_ASSERT_TRUE(ioopm_remove_from_cart(db, 1, name, 10)); // remove all of them
     ioopm_db_destroy(db);
 }
@@ -175,8 +165,8 @@ void test_add_remove_cart()
 void test_add_remove_a_few_cart()
 {
     db_t *db = ioopm_db_create();
-    char *name = "Lexie";
-    char *description = "Cool kid";
+    char *name = strdup("Lexie");
+    char *description = strdup("Cool kid");
     int price = 55;
 
     ioopm_add_merchandise(db, strdup(name), strdup(description), price);
@@ -185,21 +175,23 @@ void test_add_remove_a_few_cart()
     ioopm_add_to_cart(db, 1, name, 10);
     CU_ASSERT_TRUE(ioopm_remove_from_cart(db, 1, name, 5)); // remove a few of them
     ioopm_db_destroy(db);
+    free(name);
+    free(description);
 }
 
 void test_add_remove_multiple_cart()
 {
     db_t *db = ioopm_db_create();
-    char *name = "Lexie";
-    char *description = "Cool kid";
+    char *name = strdup("Lexie");
+    char *description = strdup("Cool kid");
     int price = 55;
 
-    char *name2 = "Vincent";
-    char *description2 = "meh";
+    char *name2 = strdup("Vincent");
+    char *description2 = strdup("meh");
     int price2 = 52;
 
-    ioopm_add_merchandise(db, strdup(name), strdup(description), price);
-    ioopm_add_merchandise(db, strdup(name2), strdup(description2), price2);
+    ioopm_add_merchandise(db, name, description, price);
+    ioopm_add_merchandise(db, name2, description2, price2);
 
     ioopm_cart_create(db); //Automagically asigns a cart 1 since not other carts exist.
     ioopm_add_to_cart(db, 1, name, 10);
@@ -235,7 +227,6 @@ int main()
     // copy a line below and change the information test_merch_create
     if (
         (CU_add_test(my_test_suite, "Test creating a database", test_db_create) == NULL) ||
-        (CU_add_test(my_test_suite, "Test creating merchandise", test_merch_create) == NULL) ||
         (CU_add_test(my_test_suite, "Test adding merchandise to database", test_add_merchandise) == NULL) ||
         (CU_add_test(my_test_suite, "Test removing merchandise to database", test_remove_merchandise) == NULL) ||
         (CU_add_test(my_test_suite, "Test changing name of merchandise", test_edit_name_merchandise) == NULL) ||
