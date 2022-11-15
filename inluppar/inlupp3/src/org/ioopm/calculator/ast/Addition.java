3baeebed0bead51ab570507bb6c579a06fda2d62
Anik Uddin <anik.uddin.2051@student.uu.se>
2022-11-15 15:58:52 +0100

    lall

--- inluppar/inlupp3/src/org/ioopm/calculator/ast/Addition.java
+++ inluppar/inlupp3/src/org/ioopm/calculator/ast/Addition.java
@@ -1,5 +1,7 @@
 package inluppar.inlupp3.src.org.ioopm.calculator.ast;
 
 public class Addition extends Binary {
-    
+    public Addition( SymbolicExpression lhs, SymbolicExpression rhs){
+        super(lhs, rhs);
+    }
 }
--- inluppar/inlupp3/src/org/ioopm/calculator/ast/Binary.java
+++ inluppar/inlupp3/src/org/ioopm/calculator/ast/Binary.java
@@ -3,4 +3,17 @@ package inluppar.inlupp3.src.org.ioopm.calculator.ast;
 public abstract class Binary {
     private SymbolicExpression lhs = null;
     private SymbolicExpression rhs = null;
+
+    public Binary(SymbolicExpression lhs, SymbolicExpression rhs) {
+
+        this.lhs = lhs;
+        this.rhs = rhs;
+
+    public SymbolicExpression getlhs(){
+        return this.lhs;
+    }
+    public SymbolicExpression getrhs(){
+        return this.rhs;
+    }
 }
+
