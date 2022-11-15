2ec27e5e97e7f72ed03a00fd9d160c2c9e6cb45d
Anik Uddin <anik.uddin.2051@student.uu.se>
2022-11-15 15:59:34 +0100

    lall2

--- inluppar/inlupp3/src/org/ioopm/calculator/ast/Addition.java
+++ inluppar/inlupp3/src/org/ioopm/calculator/ast/Addition.java
@@ -1,7 +1,12 @@
 package inluppar.inlupp3.src.org.ioopm.calculator.ast;
 
 public class Addition extends Binary {
-    public Addition( SymbolicExpression lhs, SymbolicExpression rhs){
+    public Addition(SymbolicExpression lhs, SymbolicExpression rhs){
         super(lhs, rhs);
     }
+    public String getName(){
+        return ("+");
+
+    }
+
 }
