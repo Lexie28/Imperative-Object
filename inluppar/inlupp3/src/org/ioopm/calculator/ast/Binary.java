ea119e22e3ef996a8723163b7b54cf5081f6e9d3
Alexandra Dahlberg <alexandradahlberg@LAPTOP-230VP7EK.localdomain>
2022-11-15 15:22:10 +0100

    LOLLL

--- inluppar/inlupp3/src/org/ioopm/calculator/ast/Binary.java
+++ inluppar/inlupp3/src/org/ioopm/calculator/ast/Binary.java
@@ -1,6 +1,6 @@
 package inluppar.inlupp3.src.org.ioopm.calculator.ast;
 
-public class Binary {
+public abstract class Binary {
     private SymbolicExpression lhs = null;
     private SymbolicExpression rhs = null;
 }
--- inluppar/inlupp3/src/org/ioopm/calculator/ast/Unary.java
+++ inluppar/inlupp3/src/org/ioopm/calculator/ast/Unary.java
@@ -1,5 +1,5 @@
 package inluppar.inlupp3.src.org.ioopm.calculator.ast;
 
-public class Unary {
+public abstract class Unary {
     
 }
