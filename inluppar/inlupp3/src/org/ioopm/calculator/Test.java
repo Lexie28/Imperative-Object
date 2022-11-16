dc2a793beae49265279bcdb3d8cdf3ba15618ce6
Anik Uddin <anik.uddin.2051@student.uu.se>
2022-11-16 13:45:43 +0100

    added a test i test file

--- inluppar/inlupp3/src/org/ioopm/calculator/Test.java
+++ inluppar/inlupp3/src/org/ioopm/calculator/Test.java
@@ -7,5 +7,8 @@ public class Test {
     public static void main(String[] args) {
         SymbolicExpression m = new Multiplication(new Addition(new Constant(5), new Variable("x")), new Constant(2));
         System.out.println("(5 + x) * 2 ==> " + m);
+
+        SymbolicExpression y = new Division(new Subtraction(new Constant(5), new Variable ("z")), new Constant(5)); // writing test by anik
+        System.out.println("(5 - z) / 5 ==> " + y);
     }
 }
