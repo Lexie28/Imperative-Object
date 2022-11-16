9aee5b97dd42a4e7bee97dc92fc3324b0126a6d6
Anik Uddin <anik.uddin.2051@student.uu.se>
2022-11-16 14:34:01 +0100

    lall

--- inluppar/inlupp3/src/org/ioopm/calculator/Test.java
+++ inluppar/inlupp3/src/org/ioopm/calculator/Test.java
@@ -5,19 +5,26 @@ import inluppar.inlupp3.src.org.ioopm.calculator.ast.*;
 
 public class Test {
     
+    public void testPrinting(String expected, SymbolicExpression e) {
+        if (expected.equals("" + e)) {
+            System.out.println("Passed: " + e);
+        } else {
+            System.out.println("Error: expected '" + expected + "' but got '" + e + "'");
+        }
+    }
 
     public static void main(String[] args) {
         
         
         
-        /*
+        
         SymbolicExpression m = new Multiplication(new Addition(new Constant(5), new Variable("x")), new Constant(2));
         System.out.println("(5 + x) * 2 ==> " + m);
 
         SymbolicExpression y = new Division(new Subtraction(new Constant(5), new Variable ("z")), new Constant(5)); // writing test by anik
         System.out.println("(5 - z) / 5 ==> " + y);
-        */
         
-         testPrinting("(5 + x) * 2", m);    
+        
+        
     }
 }
--- inluppar/inlupp3/src/org/ioopm/calculator/ast/testPrinting.java
+++ inluppar/inlupp3/src/org/ioopm/calculator/ast/testPrinting.java
@@ -1,12 +1,14 @@
 package inluppar.inlupp3.src.org.ioopm.calculator.ast;
 
 
-public class test_Printing{
-    public void testPrinting(String expected, SymbolicExpression e) {
+public class testPrinting{
+    
+    public testPrinting(String expected, SymbolicExpression e) {
         if (expected.equals("" + e)) {
             System.out.println("Passed: " + e);
         } else {
             System.out.println("Error: expected '" + expected + "' but got '" + e + "'");
         }
     }
+    
 }
