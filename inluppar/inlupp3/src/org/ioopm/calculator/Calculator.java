f7545a05c8c34be79a3f679749af6fe11b64678a
Lexie28 <alexandradahlberg1@gmail.com>
2022-11-21 16:36:44 +0100

    idklol

--- inluppar/inlupp3/src/org/ioopm/calculator/Calculator.java
+++ inluppar/inlupp3/src/org/ioopm/calculator/Calculator.java
@@ -1,11 +1,22 @@
 package inluppar.inlupp3.src.org.ioopm.calculator;
 
 import inluppar.inlupp3.src.org.ioopm.calculator.ast.*;
+import inluppar.inlupp3.src.org.ioopm.calculator.parser.CalculatorParser;
 
 public class Calculator {
     
     public static void main(String[] args) {
-        SymbolicExpression a = new Constant(5);
-        System.out.println("Hello world!");
+        final CalculatorParser parser = new CalculatorParser();
+        final Environment vars = new Environment();
+
+        while (true) {
+            String input = System.console().readLine();
+            SymbolicExpression ob = parser //feeding input into parser?
+            if (ob.isCommand())
+            {
+                
+            }
+
+        }
     }
 }
--- inluppar/inlupp3/src/org/ioopm/calculator/ast/SyntaxErrorException.java
+++ inluppar/inlupp3/src/org/ioopm/calculator/ast/SyntaxErrorException.java
@@ -1,5 +1,8 @@
 package inluppar.inlupp3.src.org.ioopm.calculator.ast;
 
-public class SyntaxErrorException {
+public class SyntaxErrorException extends RuntimeException {
+    public SyntaxErrorException (String errormessage) {
+        super(errormessage);
+    }
     
 }
