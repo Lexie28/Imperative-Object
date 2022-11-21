9164cb3bbc69cc034add8922d886792f25b5a4b3
Lexie28 <alexandradahlberg1@gmail.com>
2022-11-21 20:22:16 +0100

    Lol

--- inluppar/inlupp3/src/org/ioopm/calculator/Calculator.java
+++ inluppar/inlupp3/src/org/ioopm/calculator/Calculator.java
@@ -1,5 +1,7 @@
 package inluppar.inlupp3.src.org.ioopm.calculator;
 
+import java.io.IOException;
+
 import inluppar.inlupp3.src.org.ioopm.calculator.ast.*;
 import inluppar.inlupp3.src.org.ioopm.calculator.parser.CalculatorParser;
 
@@ -8,6 +10,7 @@ public class Calculator {
     public static void main(String[] args) {
         final CalculatorParser parser = new CalculatorParser();
         final Environment vars = new Environment();
+        final Stats stats = new Stats();
         
         while (true) {
             String input = System.console().readLine();
@@ -27,15 +30,23 @@ public class Calculator {
                 } 
                 else
                 {
+                    stats.addExpression();
                     SymbolicExpression evaluatedob = ob.eval(vars);
                     System.out.println("" + evaluatedob);
                     vars.put(new Variable("ans"), evaluatedob);
+                    if (evaluatedob.isConstant()) {
+                        stats.addFullEval();
+                        //kom ihåg att här adda till både full o succcess eval här då en full eval inkl. success eval
+                    } else {
+                        stats.addSuccEval();
+                    }
                 }
-            } catch (Exception e) {
-                System.out.println("Error!" + e);
             }
-            
-
+             catch (Exception e) {
+                stats.addExpression();
+                System.out.println(e.getMessage());
+            }
         }
+        stats.printstats();
     }
 }
---
+++ inluppar/inlupp3/src/org/ioopm/calculator/Stats.java
@@ -0,0 +1,33 @@
+package inluppar.inlupp3.src.org.ioopm.calculator;
+
+public class Stats {
+    public int expressions;
+    public int fulleval;
+    public int successeval;
+
+    public Stats() {
+        this.expressions = 0;
+        this.fulleval = 0;
+        this.successeval = 0;
+    }
+
+    public void addExpression() {
+        this.expressions += 1;
+    }
+
+    public void addFullEval() {
+        this.fulleval += 1;
+        this.successeval += 1;
+    }
+
+    public void addSuccEval() {
+        this.successeval += 1;
+    }
+    
+    public void printstats()
+    {
+        System.out.println("Expressions entered:" + this.expressions);
+        System.out.println("Fully evaluated:" + this.fulleval);
+        System.out.println("Successfully evaluated:" + this.successeval);
+    }
+}
