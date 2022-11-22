b646ab57f4f5640c1c89d3a601ed12715df49849
Sebastian <sebastian.akerhielm.8786@student.uu.se>
2022-11-22 15:55:24 +0100

    test constant

---
+++ inluppar/inlupp4/src/TestConstant.java
@@ -0,0 +1,56 @@
+import org.ioopm.calculator.ast.*;
+
+import org.junit.Test;
+
+
+public class TestConstant {
+    Constant c1 = new Constant(2.0);
+    Constant c2 = new Constant(2.0);
+
+    @Test 
+    public void testGetValue() {
+        assert 2.0 == c1.getValue();
+    }
+
+    @Test
+    public void testIsConstant() {
+        assert c1.isConstant() == true;
+    }
+
+    @Test
+    public void testGetName() {
+        try {
+            c1.getName();
+            assert false;
+        } catch(RuntimeException e) {
+            assert true;
+        }
+    }
+
+    @Test
+    public void testIsCommand() {
+        assert false == c1.isCommand();
+    }
+
+    @Test
+    public void testPriority() {
+        assert 200 == c1.getPriority();
+    }
+
+    @Test
+    public void testToString() {
+        assert "2.0".equals(c1.toString());
+    }
+
+    @Test
+    public void testEquals() {
+        assert c1.equals(c2);
+    }
+
+    @Test
+    public void testEval() {
+        Environment vars = new Environment();
+        assert 2.0 == c1.eval(vars).getValue();
+    }
+}
+
