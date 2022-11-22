74f4223795d4558edc849208c8d4d06b027dde5d
alexandra <alexandradahlberg1@gmail.com>
2022-11-22 15:57:37 +0100

    teehee

---
+++ inluppar/inlupp4/src/TestVariable.java
@@ -0,0 +1,60 @@
+import org.ioopm.calculator.ast.*;
+
+import org.junit.Test;
+
+public class TestVariable {
+    Variable a = new Variable("x");
+    Variable b = new Variable("x");
+
+    @Test
+    public void testGetValue() {
+        try {
+            a.getValue();
+            assert false;
+        } catch(RuntimeException e) {
+            assert true;
+        }
+    }
+
+    @Test
+    public void testIsConstant() {
+        assert false == a.isConstant();
+    }
+
+    @Test
+    public void testGetName() {
+        try {
+            a.getName();
+            assert false;
+        }
+            catch(RuntimeException e) {
+                assert true;
+            }
+    }
+
+    @Test
+    public void testIsCommand() {
+        assert false == a.isCommand();
+    }
+
+    @Test
+    public void testPriority() {
+        assert 200 == a.getPriority();
+    }
+
+    @Test
+    public void testToString() {
+        assert "x".equals(a.toString());
+    }
+
+    @Test
+    public void testEquals() {
+        assert a.equals(b);
+    }
+
+    @Test
+    public void testEval() {
+        Environment vars = new Environment();
+        assert b.equals(a.eval(vars));
+    }
+}
