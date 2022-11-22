0f761ef9d7cf910fd6497bda866cb3fc7d6f8a04
alexandra <alexandradahlberg1@gmail.com>
2022-11-22 15:50:13 +0100

    yay

---
+++ inluppar/inlupp4/src/TestAssignment.java
@@ -0,0 +1,57 @@
+import org.ioopm.calculator.ast.*;
+
+import org.junit.Test;
+
+public class TestAssignment {
+    Constant c1 = new Constant(2.0);
+    Variable x = new Variable("x");
+    Assignment a = new Assignment(c1, x);
+    Assignment a2 = new Assignment(c1, x); //TODO?? testa equals
+    //Addition a2 = new Addition(c2, c1);
+
+    @Test
+    public void testGetValue() {
+        try {
+            a.getValue();
+            assert false;
+        } catch(RuntimeException e) {
+            assert true;
+        }
+    }
+
+    @Test
+    public void testIsConstant() {
+        assert false == a.isConstant();
+    }
+
+    @Test
+    public void testGetName() {
+        assert "=".equals(a.getName());
+    }
+
+    @Test
+    public void testIsCommand() {
+        assert false == a.isCommand();
+    }
+
+    @Test
+    public void testPriority() {
+        assert 0 == a.getPriority();
+    }
+
+    @Test
+    public void testToString() {
+        assert "2.0 = x".equals(a.toString());
+    }
+
+    @Test
+    public void testEquals() {
+        assert a.equals(a2);
+    }
+
+    @Test
+    public void testEval() {
+        Environment vars = new Environment();
+        assert 2.0 == a.eval(vars).getValue();
+    }
+}
---
+++ inluppar/inlupp4/src/TestSubtraction.java
@@ -0,0 +1,56 @@
+import org.ioopm.calculator.ast.*;
+
+import org.junit.Test;
+
+public class TestSubtraction {
+    Constant c1 = new Constant(5.0);
+    Constant c2 = new Constant(2.0);
+    Subtraction a = new Subtraction(c1, c2);
+    Subtraction a2 = new Subtraction(c2, c1);
+
+    @Test
+    public void testGetValue() {
+        try {
+            a.getValue();
+            assert false;
+        } catch(RuntimeException e) {
+            assert true;
+        }
+    }
+
+    @Test
+    public void testIsConstant() {
+        assert false == a.isConstant();
+    }
+
+    @Test
+    public void testGetName() {
+        assert "-".equals(a.getName());
+    }
+
+    @Test
+    public void testIsCommand() {
+        assert false == a.isCommand();
+    }
+
+    @Test
+    public void testPriority() {
+        assert 50 == a.getPriority();
+    }
+
+    @Test
+    public void testToString() {
+        assert "5.0 - 2.0".equals(a.toString());
+    }
+
+    @Test
+    public void testEquals() {
+        assert a.equals(a2);
+    }
+
+    @Test
+    public void testEval() {
+        Environment vars = new Environment();
+        assert 3.0 == a.eval(vars).getValue();
+    }
+}
