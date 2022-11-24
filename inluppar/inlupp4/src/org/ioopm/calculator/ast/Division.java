cbff177d41664627f1e8aef69b7f7d8f018e709c
alexandra <alexandradahlberg1@gmail.com>
2022-11-24 17:04:32 +0100

    llo

--- inluppar/inlupp4/src/org/ioopm/calculator/ast/Division.java
+++ inluppar/inlupp4/src/org/ioopm/calculator/ast/Division.java
@@ -3,7 +3,7 @@ package org.ioopm.calculator.ast;
 public class Division extends Binary {
 
     public Division(SymbolicExpression lhs, SymbolicExpression rhs){
-        super(lhs, rhs);
+        super(getName(), lhs, rhs);
     }
     
     /**
