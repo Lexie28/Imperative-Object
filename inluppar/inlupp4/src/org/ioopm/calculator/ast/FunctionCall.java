a15b0a842f9702e639eb2928850f4329eb17380a
Sebastian <sebastian.akerhielm.8786@student.uu.se>
2022-12-02 13:34:19 +0100

    yes

--- inluppar/inlupp4/src/org/ioopm/calculator/ast/FunctionCall.java
+++ inluppar/inlupp4/src/org/ioopm/calculator/ast/FunctionCall.java
@@ -1,5 +1,19 @@
 package org.ioopm.calculator.ast;
 
-public class FunctionCall {
-    
+public class FunctionCall extends SymbolicExpression{
+
+    String identifier;
+
+    public FunctionCall(String name, Variable... args) {
+        super(name);
+    }
+
+
+
+    @Override
+    public SymbolicExpression accept(Visitor v) {
+        return v.visit(this);
+    }
+
+
 }
--- inluppar/inlupp4/src/org/ioopm/calculator/ast/FunctionDeclaration.java
+++ inluppar/inlupp4/src/org/ioopm/calculator/ast/FunctionDeclaration.java
@@ -1,5 +1,24 @@
 package org.ioopm.calculator.ast;
 
-public class FunctionDeclaration {
+public class FunctionDeclaration extends SymbolicExpression {
     
+    String identifier;
+    Variable[] args;
+    Sequence seq;
+
+    public FunctionDeclaration(String name,Sequence sequence, Variable... args ) {
+        super(name);
+        this.identifier = name;
+        this.seq = sequence;
+        this.args = args;
+    }
+
+
+    @Override
+    public SymbolicExpression accept(Visitor v) {
+        return v.visit(this);
+    }
+
+    
+
 }
