149fffc49e8af13240a926f170c06d315f321bc9
alexandra <alexandradahlberg1@gmail.com>
2022-12-06 09:39:09 +0100

    hej

--- inluppar/inlupp4/src/TestNegation.java
+++ inluppar/inlupp4/src/TestNegation.java
@@ -9,6 +9,11 @@ public class TestNegation {
     Negation n = new Negation(c);
     Negation n2 = new Negation(c);
 
+    Constant c2 = new Constant(1);
+    Constant c3 = new Constant(-1);
+    Constant c4 = new Constant(-1);
+    Negation n3 = new Negation(c2);
+
     @Test 
     public void testGetValue() {
         try{
@@ -22,6 +27,7 @@ public class TestNegation {
     @Test
     public void testIsConstant() {
         assert n.isConstant() == false;
+        assert n3.isConstant() == false;
     }
 
     @Test
@@ -32,16 +38,19 @@ public class TestNegation {
     @Test
     public void testIsCommand() {
         assert false == n.isCommand();
+        assert false == n3.isCommand();
     }
 
     @Test
     public void testPriority() {
         assert 150 == n.getPriority();
+        assert 150 == n3.getPriority();
     }
 
     @Test
     public void testToString() {
         assert "- 2.0".equals(n.toString());
+        assert "- 1.0".equals(n3.toString());
     }
 
     @Test
@@ -55,5 +64,13 @@ public class TestNegation {
         Environment funcs = new Environment();
         EvaluationVisitor ev = new EvaluationVisitor();
         assert c1.equals(ev.evaluate(n, vars, funcs));
+    }
+
+    @Test
+    public void testEval2() {
+        Environment vars = new Environment();
+        Environment funcs = new Environment();
+        EvaluationVisitor ev = new EvaluationVisitor();
+        assert c3.equals(ev.evaluate(n3, vars, funcs));
     }
 }
